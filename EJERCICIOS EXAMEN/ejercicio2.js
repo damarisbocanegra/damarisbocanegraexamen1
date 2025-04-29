9c6e1c1d84c6693de81d55ff3ff3ee4b7f1916a4
O23233 <O23233@lab.aca>
2025-04-28 21:42:43 -0500

    Ejercicio terminal

---
+++ EJERCICIOS EXAMEN/ejercicio2.js
@@ -0,0 +1,15 @@
+document.getElementById('addButton').addEventListener('click', function () {
+    const imageUrl = document.getElementById('imageUrl').value;
+    if (imageUrl) {
+        const img = document.createElement('img');
+        img.src = imageUrl;
+        img.alt = 'Imagen de la galería';
+        
+        const gallery = document.getElementById('gallery');
+        gallery.appendChild(img);
+        
+        document.getElementById('imageUrl').value = ''; // Limpiar el campo de entrada
+    } else {
+        alert('Por favor, ingrese una URL de imagen válida.');
+    }
+});
